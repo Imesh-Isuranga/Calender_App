3b4afe55bd154118c83956fd6e273a5e54bf716d
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-05-15 15:23:15 +0530

    Created Calender

---
+++ lib/calender.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calender extends StatefulWidget {
+  const Calender({super.key});
+
+  @override
+  State<Calender> createState() => _CalenderState();
+}
+
+class _CalenderState extends State<Calender> {
+  DateTime today = DateTime.now();
+
+  void _onDaySelected(DateTime day, DateTime focusDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Xibet Calender'),
+      ),
+      body: Container(
+          child: Column(
+        children: [
+          const Text('Xibet'),
+          Container(
+            child: TableCalendar(
+              rowHeight: 43,
+              headerStyle:
+                  HeaderStyle(formatButtonVisible: false, titleCentered: true),
+              availableGestures: AvailableGestures.all,
+              selectedDayPredicate: (day) => isSameDay(day, today),
+              focusedDay: today,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 10, 16),
+              onDaySelected: _onDaySelected,
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+}
+
+
---
+++ lib/splash.dart
@@ -0,0 +1,59 @@
+import 'package:calender_app/calender.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 2),
+    vsync: this,
+  )..repeat(reverse: true);
+
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.fastOutSlowIn,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+    _backToHome();
+  }
+
+  _backToHome() async {
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Calender(),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ScaleTransition(
+          scale: _animation,
+          child: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: FlutterLogo(size: 150.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
