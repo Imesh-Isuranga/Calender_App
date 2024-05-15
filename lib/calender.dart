d264c5c3f9225851f788ec199e24d965f6a3717d
Imesh Isuranga <imeshisuranga00@gmail.com>
2024-05-16 04:02:11 +0530

    Added Local notifications and shared preferences

--- lib/calender.dart
+++ lib/calender.dart
@@ -1,5 +1,8 @@
 import 'package:calender_app/event.dart';
+import 'package:calender_app/notificationservice.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 class Calender extends StatefulWidget {
@@ -10,78 +13,253 @@ class Calender extends StatefulWidget {
 }
 
 class _CalenderState extends State<Calender> {
-  DateTime today = DateTime.now();
-  TimeOfDay selectedTime = TimeOfDay.now();
-  TimeOfDay? timeOfDay;
+  TimeOfDay? selectedTime = TimeOfDay.now();
+  DateTime selectedDateTime = DateTime.now();
+  int secondsDifferencefor_15min = 0;
+  int secondsDifferencefor_1h = 0;
+  int secondsDifferencefor_1d = 0;
+  int secondsDifferencefor_ontime = 0;
+  int id = 0;
+  List<String> stringList = [];
 
-  TextEditingController _eventController = TextEditingController();
+  final TextEditingController _eventController = TextEditingController();
   Map<DateTime, List<Event>> events = {};
+
+  Future<void> _loadStringList() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      stringList = prefs.getStringList('myStringList') ?? [];
+    });
+  }
+
+  Future<void> _saveStringList(List<String> list) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('myStringList', list);
+  }
+
+  void addList() {
+    setState(() {
+      stringList.add(
+          '${selectedDateTime.year}/${selectedDateTime.month}/${selectedDateTime.day} | ${selectedDateTime.hour}:${selectedDateTime.minute} - ${_eventController.text}');
+      _saveStringList(stringList);
+    });
+  }
 
   void _onDaySelected(DateTime day, DateTime focusDay) {
     setState(() {
-      today = day;
+      selectedDateTime = day;
     });
+  }
+
+  LocalNotifications notificationService = LocalNotifications();
+
+  @override
+  void initState() {
+    _loadStringList();
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Xibet Calender'),
+        title: const Text(
+          'Xibet Calender',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 15, 3, 66),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        showDialog(
-          context: context,
-          builder: (context) {
-            return AlertDialog(
-              scrollable: true,
-              title: Text('Event Creator'),
-              content: Padding(
-                padding: EdgeInsets.all(8),
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: _eventController,
-                    ),
-                    ElevatedButton(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                scrollable: true,
+                title: const Text('Event Creator'),
+                content: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'Task',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextField(
+                        controller: _eventController,
+                      ),
+                      const Padding(
+                          padding: EdgeInsets.symmetric(vertical: 10)),
+                      ElevatedButton(
                         onPressed: () async {
-                          timeOfDay = await showTimePicker(
-                              context: context,
-                              initialTime: selectedTime,
-                              initialEntryMode: TimePickerEntryMode.dial);
+                          selectedTime = await showTimePicker(
+                            context: context,
+                            initialTime: selectedTime!,
+                            initialEntryMode: TimePickerEntryMode.dial,
+                          );
+
+                          if (selectedTime != null) {
+                            setState(() {
+                              id++;
+
+                              selectedDateTime = DateTime(
+                                selectedDateTime.year,
+                                selectedDateTime.month,
+                                selectedDateTime.day,
+                                selectedTime!.hour,
+                                selectedTime!.minute,
+                              );
+
+                              DateTime now = DateTime.now();
+                              // Calculate the difference in seconds
+                              Duration difference =
+                                  selectedDateTime.difference(now);
+                              // Get the difference in seconds
+                              secondsDifferencefor_15min =
+                                  difference.inSeconds - (15 * 60);
+                              secondsDifferencefor_1h =
+                                  difference.inSeconds - (60 * 60);
+                              secondsDifferencefor_1d =
+                                  difference.inSeconds - (60 * 60 * 24);
+                              secondsDifferencefor_ontime =
+                                  difference.inSeconds;
+                            });
+                          }
                         },
-                        child: Text('Choose Time'))
-                  ],
+                        child: const Text('Choose Time'),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              actions: [
-                ElevatedButton(onPressed: () {
-                  
-                }, child: Text('Submit'))
-              ],
-            );
-          },
-        );
-      }),
-      body: Container(
-          child: Column(
+                actions: [
+                  Column(
+                    children: [
+                      ElevatedButton(
+                        onPressed: () async {
+                          LocalNotifications.showScheduleNotification(
+                            id: id,
+                            title: "Xibet",
+                            body: _eventController.text,
+                            duration: secondsDifferencefor_15min <= 0
+                                ? secondsDifferencefor_15min = 0
+                                : secondsDifferencefor_15min,
+                            payload: "This is schedule data",
+                          );
+                          addList();
+                          _eventController.clear();
+                          Navigator.pop(context);
+                        },
+                        child: const Text('15 mins before'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          LocalNotifications.showScheduleNotification(
+                            id: id,
+                            title: "Xibet",
+                            body: _eventController.text,
+                            duration: secondsDifferencefor_1h <= 0
+                                ? secondsDifferencefor_1h = 0
+                                : secondsDifferencefor_1h,
+                            payload: "This is schedule data",
+                          );
+                          addList();
+                          _eventController.clear();
+                          Navigator.pop(context);
+                        },
+                        child: const Text('1 hour before'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          LocalNotifications.showScheduleNotification(
+                            id: id,
+                            title: "Xibet",
+                            body: _eventController.text,
+                            duration: secondsDifferencefor_1d <= 0
+                                ? secondsDifferencefor_1d = 0
+                                : secondsDifferencefor_1d,
+                            payload: "This is schedule data",
+                          );
+                          addList();
+                          _eventController.clear();
+                          Navigator.pop(context);
+                        },
+                        child: const Text('1 day before'),
+                      ),
+                      ElevatedButton(
+                        onPressed: () async {
+                          LocalNotifications.showScheduleNotification(
+                            id: id,
+                            title: "Xibet",
+                            body: _eventController.text,
+                            duration: secondsDifferencefor_ontime,
+                            payload: "This is schedule data",
+                          );
+                          addList();
+                          _eventController.clear();
+                          Navigator.pop(context);
+                        },
+                        child: const Text('On-time'),
+                      ),
+                    ],
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Text('+'),
+      ),
+      body: ListView(
         children: [
-          const Text('Xibet'),
           Container(
             child: TableCalendar(
               rowHeight: 43,
-              headerStyle:
-                  HeaderStyle(formatButtonVisible: false, titleCentered: true),
+              headerStyle: const HeaderStyle(
+                formatButtonVisible: false,
+                titleCentered: true,
+              ),
               availableGestures: AvailableGestures.all,
-              selectedDayPredicate: (day) => isSameDay(day, today),
-              focusedDay: today,
+              selectedDayPredicate: (day) => isSameDay(day, selectedDateTime),
+              focusedDay: selectedDateTime,
               firstDay: DateTime.utc(2010, 10, 16),
               lastDay: DateTime.utc(2030, 10, 16),
               onDaySelected: _onDaySelected,
             ),
           ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: stringList.length,
+            itemBuilder: (context, index) {
+              return Dismissible(
+                key: UniqueKey(), // Unique key for each item
+                onDismissed: (direction) {
+                  setState(() {
+                    stringList.removeAt(index); // Remove the item from the list
+                    LocalNotifications.cancel(index + 1);
+                    _saveStringList(stringList); // Save the updated list
+                  });
+                },
+                background: Container(
+                  color: Colors.red,
+                  child: const ListTile(
+                    leading: Icon(Icons.delete, color: Colors.white),
+                  ),
+                ),
+                child: Card(
+                  elevation: 2,
+                  child: ListTile(
+                    title: Text(stringList[index]),
+                  ),
+                ),
+              );
+            },
+          ),
         ],
-      )),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,18 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Xibet Calender',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 58, 85, 183)),
+        floatingActionButtonTheme: const FloatingActionButtonThemeData(
+          backgroundColor: Color.fromARGB(255, 7, 2, 108),
+          foregroundColor: Colors.white
+        ),
         useMaterial3: true,
       ),
       home: const SplashScreen(),
